cfec19503c30ae2619d9544e048765823baf5690
Aliaksandr Veramkovich <averamkovich@allot.com>
2022-04-19 17:19:20 +0300

    Lesson 16: IO - NIO

--- src/my/nio/ReadMeFile.java
+++ src/my/nio/ReadMeFile.java
@@ -1,8 +1,8 @@
 package my.nio;
 
 import java.io.IOException;
-import java.nio.Buffer;
 import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.channels.SeekableByteChannel;
 import java.nio.file.Files;
@@ -22,28 +22,35 @@ public class ReadMeFile {
     }
 
     public static void saveToReadMeFile2(String s) throws IOException {
-        Files.newByteChannel(
+        try (
+                final SeekableByteChannel seekableByteChannel = Files.newByteChannel(
                         Paths.get("readme.txt"),
                         StandardOpenOption.CREATE,
                         StandardOpenOption.WRITE
                 )
-                .write(ByteBuffer.allocate(s.length()).put(s.getBytes()));
+        ) {
+            seekableByteChannel
+                    .write(ByteBuffer.allocate(s.length()).put(s.getBytes()).position(0));
+        }
     }
 
     public static String readFromReadMeFile() throws IOException {
-        String s;
-        ByteBuffer byteBuffer = ByteBuffer.allocate(10);
-        int size = Files.newByteChannel(Paths.get("readme.txt"))
-                .read(byteBuffer);
-        byte[] bytes = new byte[size];
-        byteBuffer.get(bytes);
-        s = new String(bytes);
-        return s;
+        final Path path = Paths.get("readme.txt");
+        try (
+                final FileChannel seekableByteChannel = (FileChannel) Files.newByteChannel(path)
+        ) {
+            MappedByteBuffer byteBuffer = seekableByteChannel.map(FileChannel.MapMode.READ_ONLY, 0, Files.size(path));
+            byte[] bytes = new byte[byteBuffer.capacity()];
+            byteBuffer.get(bytes);
+            return new String(bytes);
+        }
     }
 
     public static void main(String[] args) throws IOException {
-        String s = "Hello World";
+        String s = "Hello World - I love Java";
         saveToReadMeFile2(s);
+        String s2 = readFromReadMeFile();
+        System.out.println(s2);
     }
 
 }
