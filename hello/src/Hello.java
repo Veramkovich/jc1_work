ededb29787703ecc41c9a32b3efe6d5b1ac37a4b
Aliaksandr Veramkovich <averamkovich@allot.com>
2023-03-14 15:21:03 +0300

    Lesson 2: Class, object, method

---
+++ hello/src/Hello.java
@@ -0,0 +1,11 @@
+// This is my first java program
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.print(args[0] + " " + args[1] + " " + args[2]);
+        int count = 0;
+        //TODO: run with one arg
+        //FIXME: test
+    }
+}
+
