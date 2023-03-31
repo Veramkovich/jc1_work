f9caf360d8885eff6d60202003cd5c33c8d3c89c
Aliaksandr Veramkovich <averamkovich@allot.com>
2023-03-31 11:04:22 +0300

    Lesson 7: while, do-while, for and for-each loops; arrays - live coding

--- hello/src/part1/task6/Main.java
+++ hello/src/part1/task6/Main.java
@@ -1,9 +1,12 @@
 package part1.task6;
 
+import lesson7.Automat;
+import lesson7.Product;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Automat automat = new Automat();
     }
 
 }
