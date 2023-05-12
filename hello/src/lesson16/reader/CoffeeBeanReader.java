f626c9ccdab446e8aec0c3b04078a83e97e47dde
Aliaksandr Veramkovich <averamkovich@allot.com>
2023-05-12 12:23:03 +0300

    Lesson 16: Input and Output, File

---
+++ hello/src/lesson16/reader/CoffeeBeanReader.java
@@ -0,0 +1,24 @@
+package lesson16.reader;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+public class CoffeeBeanReader {
+
+    public Object readFromFile(String fileName) {
+        File file = new File(fileName);
+        if (!file.exists()) {
+            return null;
+        }
+        try (FileInputStream fis = new FileInputStream(file);
+             ObjectInputStream ois = new ObjectInputStream(fis)) {
+            return ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
