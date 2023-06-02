c0c46af92051c8048e3efc50e378e29f50fd1561
Aliaksandr Veramkovich <averamkovich@allot.com>
2023-06-02 14:35:20 +0300

    Lesson 22: XML, JSON, Parsers

---
+++ hello/src/lesson22/DomParserExample.java
@@ -0,0 +1,38 @@
+package lesson22;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+
+public class DomParserExample {
+
+    public static void main(String[] args) {
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder parser = factory.newDocumentBuilder();
+
+            final Document document = parser.parse(new File("c:/work/hello/resources/test.xml"));
+
+            final NodeList childNodes = document.getChildNodes();
+            for (int i = 0; i < childNodes.getLength(); i++) {
+                final Node item = childNodes.item(i);
+                System.out.println(item.getNodeName() + ": "
+                        + childNodes.item(i).getTextContent());
+                System.out.println(item.getAttributes().getNamedItem("id").getTextContent());
+            }
+
+        } catch (ParserConfigurationException | IOException | SAXException e) {
+            e.printStackTrace();
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
+    }
+
+}
